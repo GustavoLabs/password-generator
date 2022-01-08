b2b99ace58497b332cfc66ab0b6d2962faa575d7
Gustavo <gusta.rgouvea@gmail.com>
2022-01-08 14:30:45 -0300

    Added exceptions classes

---
+++ src/main/java/com/psw/generator/exception/InvalidOperationException.java
@@ -0,0 +1,7 @@
+package com.psw.generator.exception;
+
+public class InvalidOperationException extends PasswordGeneratorException{
+    public InvalidOperationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/psw/generator/exception/PasswordGeneratorException.java
@@ -0,0 +1,8 @@
+package com.psw.generator.exception;
+
+public class PasswordGeneratorException extends RuntimeException{
+
+    public PasswordGeneratorException(String message) {
+        super(message);
+    }
+}
